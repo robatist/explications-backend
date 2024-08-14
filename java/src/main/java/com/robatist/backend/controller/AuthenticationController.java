cf2e96d4a69285c684eedf434b87e52d4d6a1868
robatist <rodrigobatista836@gmail.com>
2024-08-14 15:11:49 +0100

    evol: AuthenticationController udpated
    Signed-off-by: robatist <rodrigobatista836@gmail.com>

--- java/src/main/java/com/robatist/backend/controller/AuthenticationController.java
+++ java/src/main/java/com/robatist/backend/controller/AuthenticationController.java
@@ -1,34 +1,40 @@
-package com.robatist.backend.auth;
+package com.robatist.backend.controller;
 
+import com.robatist.backend.auth.AuthenticationRequest;
+import com.robatist.backend.auth.AuthenticationResponse;
+import com.robatist.backend.auth.AuthenticationService;
+import com.robatist.backend.auth.RegisterRequest;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
+@Transactional
 @RestController
-@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
-public class AuthenticationController {
+public class AuthenticationController implements ApiController {
+
+    private static final String BASE_PATH_AUTH = "/auth";
 
     private final AuthenticationService authenticationService;
 
-    @PostMapping("/register")
+    @PostMapping(BASE_PATH_AUTH + "/register")
     public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authenticationService.register(request));
     }
 
-    @PostMapping("/authenticate")
+    @PostMapping(BASE_PATH_AUTH + "/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
         return ResponseEntity.ok(authenticationService.authenticate(request));
     }
 
-    @PostMapping("/refresh-token")
+    @PostMapping(BASE_PATH_AUTH + "/refresh-token")
     public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
         authenticationService.refreshToken(request, response);
     }
