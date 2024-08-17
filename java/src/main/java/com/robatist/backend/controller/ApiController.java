24a17581b36e5e3068ce59be845c5a856b25e257
Rodrigo Batista <57587866+robatist@users.noreply.github.com>
2024-08-17 15:22:40 +0100

    Common integration (#4)
    * evol: parent pom changed to the common project
    * evol: AuthenticationController udpated

--- java/src/main/java/com/robatist/backend/controller/ApiController.java
+++ java/src/main/java/com/robatist/backend/controller/ApiController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping(path = "/api/v1")
-@CrossOrigin(origins = "http://localhost:40000") // TODO: 24/10/2023 confirm if this change work here
+@CrossOrigin(origins = "http://localhost:40000")
 public interface ApiController {
 
 }
